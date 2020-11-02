c7e9a241d1c41ad7469db0685a765521a491d04c
ArtemON3010 <artem.turko.ITIS.2020@lpnu.ua>
2020-11-03 01:40:39 +0200

    first branch_05 commit

---
+++ Лабораторна робота №4.5/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y, R;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		cout << "R = "; cin >> R;
+		x = 6. * rand() / RAND_MAX - 3;
+		y = 6. * rand() / RAND_MAX - 3;
+		if ((y <= sqrt((R * R) - (x * x)) && x <= 0 && 0 <= y) ||
+			(y <= 0 && y >= -2 * x && y >= 2 * (x - R)))
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+		else
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+	}
+	return 0;
+}
